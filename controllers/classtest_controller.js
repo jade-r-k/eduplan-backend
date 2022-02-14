d4737780d9a28d5a00fb75b21c63b904918cbcb7
jade-r-k <jade.r.s.kelly@gmail.com>
2022-02-14 21:21:48 +0000

    post class test

--- controllers/classtest_controller.js
+++ controllers/classtest_controller.js
@@ -32,7 +32,28 @@ const getSingleClassTest = (req, res) => {
     })
 }
 
+const addClassTest = (req, res) => {
+    let classtestData = req.body
+
+    Classtest.create(classtestData)
+    .then((data) => {
+        if(data){
+            res.status(201).json(data)
+        }
+    })
+    .catch((err)=>{
+        if(err.name === "ValidationError"){
+            res.status(422).json(err)
+        }
+        else{
+            console.error(err)
+            res.status(500).json(err)
+        }
+    })
+}
+
 module.exports = {
     getAllClasstests,
-    getSingleClassTest
+    getSingleClassTest,
+    addClassTest
 }
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 require('dotenv').config()
 require('./db')()
 
-const { getAllClasstests, getSingleClassTest } = require('./controllers/classtest_controller')
+const { getAllClasstests, getSingleClassTest, addClassTest } = require('./controllers/classtest_controller')
 
 const port = process.env.PORT || 3000
 
@@ -16,6 +16,7 @@ app.use(express.json())
 /////ROUTES/////
 app.get('/classtests', getAllClasstests)
 app.get('/classtests/:id', getSingleClassTest)
+app.post('/classtests', addClassTest)
 ////////////////
 
 
